94750ab7ca956a6bda7e4933dc6d7d191ad4277d
MaximTurcko <max.turcko@yandex.ru>
2026-01-25 14:35:40 +0300

    HW5 TheSecondDiagonal has been added

--- src/hw5/homeWork4.java
+++ src/hw5/homeWork4.java
@@ -2,18 +2,28 @@ package main;
 
 public class homeWork4 {
     public static void main(String[] args) {
-        calculateResult(getArray1());
+        theFirstDiagonal(getArray1());
+        theSecondDiagonal(getArray1());
     }
 
-    public static void calculateResult(int[][] array1) {
-        int sum = 0;
+    public static void theFirstDiagonal(int[][] array1) {
+        System.out.print("Главная диагональ: ");
         for (int outer = 0; outer < array1.length; outer++) {
             for (int inner = 0; inner < array1[outer].length; inner++) {
-                if (outer == inner){
+                if (outer == inner) {
                     System.out.print(array1[outer][inner] + "\t");
                 }
             }
         }
+        System.out.println();
+    }
+
+    public static void theSecondDiagonal(int[][] array1) {
+        System.out.print("Побочная диагональ: ");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i][array1.length - 1 - i] + "\t");
+        }
+
     }
 
     public static int[][] getArray1() {
