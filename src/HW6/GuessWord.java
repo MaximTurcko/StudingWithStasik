9d214ca0a3102313c1fccb2fc5f851a928731774
MaximTurcko <max.turcko@yandex.ru>
2026-01-26 12:07:21 +0300

    HW6 GuessWord

--- src/HW6/GuessWord.java
+++ src/HW6/GuessWord.java
@@ -5,17 +5,41 @@ import java.util.Scanner;
 
 public class GuessWord {
     public static void main(String[] args) {
-    startGame();
+        startGame();
     }
 
-    public static void startGame(){
+    public static void startGame() {
         getIntrductoryMsg();
-        if (playGame()){
+        if (playGame()) {
             System.out.println("LETS PLAY GAME!");
             getInstructions();
             char[] hiddenWord = getRandomWord(words()).toCharArray();
             System.out.println(hiddenWord);
-            System.out.println(hiddenWord[1]);
+            System.out.print("Write the Letter or WORD: ");
+            String input = input().nextLine();
+            char[] guessWord = new char[hiddenWord.length];
+            for (int i = 0; i < guessWord.length; i++) {
+                guessWord[i] = hiddenWord[i];
+            }
+            while (input.length() == 0) {
+                System.out.println("You Wrote nothing. Please, try again");
+                input = input().nextLine();
+            }
+            if (input.length() == 1) {
+                char  inputChar = input.charAt(0);
+                for (int i = 0; i < hiddenWord.length; i++) {
+                    if(hiddenWord[i] == inputChar){
+                        guessWord[i] = inputChar;
+                    } else guessWord[i] ='*';
+                }
+
+
+            } else if (input.length() > 1) {
+                if (String.valueOf(hiddenWord).equalsIgnoreCase(input)) {
+                    System.out.println("YOU HAVE WON!");
+                    return;
+                } else System.out.println("You NOT to guess the WORD");
+            }
 
         } else {
             System.out.println("\nYou said NO. GOOODBYEEE!".toUpperCase());
@@ -23,13 +47,14 @@ public class GuessWord {
 
     }
 
-    public static boolean playGame(){
+    public static boolean playGame() {
         System.out.println("Would you want to play? Press 'y' to continue");
         return input().nextLine().equalsIgnoreCase("y");
 
 
     }
-    public static void getIntrductoryMsg(){
+
+    public static void getIntrductoryMsg() {
         System.out.println("*********************************************");
         System.out.println("Welcome to Guess Word Game. the PC randomly selects words and higes it.");
         System.out.println("You should guess the word. If you are successful - YOU WIN.");
@@ -37,7 +62,7 @@ public class GuessWord {
 
     }
 
-    public static void getInstructions(){
+    public static void getInstructions() {
         System.out.println("*********************************************");
         System.out.println("The AI have created a set of hidden words. Its stored these words in an array.");
         System.out.println("The random number has been generated to randomly select a word from the array.");
@@ -45,7 +70,7 @@ public class GuessWord {
         System.out.println("*********************************************");
     }
 
-    public static String[] words(){
+    public static String[] words() {
         return new String[]{
                 "Aplle", "House", "Car", "Stone", "Dragon",
                 "Vehicle", "Pineapple", "Watermelon", "Globe", "Tree",
@@ -54,11 +79,15 @@ public class GuessWord {
         };
     }
 
-    public static String getRandomWord(String[] hiddenWord){
+    public static String getRandomWord(String[] hiddenWord) {
         return hiddenWord[new Random().nextInt(0, hiddenWord.length)];
     }
 
-    public static Scanner input(){
+    public static void printGuessWord(){
+        for(el : guessWord)
+    }
+
+    public static Scanner input() {
         return new Scanner(System.in);
     }
 }
