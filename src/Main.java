84f661a091a5d9eed63e7cc6e3c6adcb606f4660
MaximTurcko <max.turcko@yandex.ru>
2026-02-05 13:37:27 +0300

    HW7 not fully complited

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import HW7.Phone;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        getPhones();
+    }
+
+    public static void getPhones(){
+        Phone iPhone = new Phone(11,"iPhone",103);
+        Phone maxPhone = new Phone(12,"maxPhone",104);
+        Phone bulbaPhone = new Phone(13,"bulbaPhone",105);
+        System.out.println(iPhone.getFullInfo());
+        System.out.println("*********************");
+        System.out.println(maxPhone.getFullInfo());
+        System.out.println("*********************");
+        System.out.println(bulbaPhone.getFullInfo());
+        System.out.println("**********************");
+        System.out.println(iPhone.receiveCall());
+        System.out.println(maxPhone.receiveCall());
+        System.out.println(bulbaPhone.receiveCall());
+        System.out.println("*********************");
+        System.out.println(iPhone.receiveCall("max",228));
+        iPhone.sendMassage("палим номер1","палим номер2");
 
     }
 }
