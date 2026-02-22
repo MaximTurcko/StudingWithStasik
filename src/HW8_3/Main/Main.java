0995172fe60890264c329601a90410889fa224dc
MaximTurcko <max.turcko@yandex.ru>
2026-02-22 16:28:52 +0300

    HW8_3 not fully complited

--- src/HW8_3/Main/Main.java
+++ src/HW8_3/Main/Main.java
@@ -2,6 +2,8 @@ package HW8_3.Main;
 
 import HW8_3.Goods.Goods;
 import HW8_3.Register.Register;
+import HW8_3.Stuff.Stuff;
+
 
 import java.util.Date;
 
@@ -9,9 +11,10 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Register[] goods1 = new Goods(1, new Date(), "Шмот", 100500),
-
-        ;
+        Register[] goods1 = new Register(
+                new Goods(1, new Date(), "Шмот", 100500),
+                new Goods(2,new Date(),"hz",1003)
+        );
         System.out.println(goods1.getInfo());
     }
 }
