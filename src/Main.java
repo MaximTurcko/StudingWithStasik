a6b6ea93cea6ebe0cc28d95a3a70190f49b5f2a1
MaximTurcko <max.turcko@yandex.ru>
2026-03-11 13:29:07 +0300

    HW10 !!!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,34 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        getBeginingWithLetters();
+    }
 
+    public static void get2TheFirstBlocks(){
+        System.out.println(getNumber().substring(0,8));
+    }
+
+    public static void getReplaceLetters(){
+        System.out.println(getNumber().replaceAll("(?<=-)[a-zA-Z]{3}(?=-)", "***"));
+    }
+
+    public static String getReplaceLetters2(){
+        String yLetters = getNumber().replaceAll("[^a-zA-Z]", "");
+        StringBuilder result = new StringBuilder(yLetters);
+        result.insert(3,'/');
+        result.insert(7,'/');
+        result.insert(9,'/');
+        System.out.println(result);
+        return result.toString();
+    }
+
+    public static void getBeginingWithLetters(){
+        StringBuilder d = new StringBuilder(getReplaceLetters2());
+        d.insert(0, "Letters:");
+        System.out.println(d);
+    }
+
+    public static String getNumber(){
+        return "1545-etd-7864-jyf-7j3r";
     }
 }
