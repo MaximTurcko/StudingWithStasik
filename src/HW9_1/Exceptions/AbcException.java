4a7910fb7f0d83ef8a079b1307ef63fed797c82d
MaximTurcko <max.turcko@yandex.ru>
2026-05-13 12:23:35 +0300

    HW10 not Complited !!!

--- src/HW9_1/Exceptions/AbcException.java
+++ src/HW9_1/Exceptions/AbcException.java
@@ -1,10 +1,7 @@
 package HW9_1.Exceptions;
 
 public class AbcException extends Exception{
-    private String massage;
-
     public AbcException(String massage) {
         super(massage);
-        this.massage = massage;
     }
 }
